a3b75a665a4228b293ab21765c3c1b4bab80d19d
Hew Ming Hong <redsuns900@hotmail.com>
2020-12-08 22:12:59 +0800

    Added Interviewer Part Project. Included some plugins from NuGet Installer.

---
+++ Interviewer/CreateFeedback.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Assignment_InterviewerForm
+{
+    public class CreateFeedback
+    {
+        private int interviewerChosenTemplateID;
+        private string interviewerFirstName;
+        private string interviewerLastName;
+        private string interviewerAddress;
+        private string interviewerPosition;
+        private string interviewerEmail;
+        private int interviewerPhoneNo;
+        private string interviewerDay;
+
+        public string InterviewerFirstName { get => interviewerFirstName; set => interviewerFirstName = value; }
+        public string InterviewerLastName { get => interviewerLastName; set => interviewerLastName = value; }
+        public string InterviewerAddress { get => interviewerAddress; set => interviewerAddress = value; }
+        public string InterviewerPosition { get => interviewerPosition; set => interviewerPosition = value; }
+        public string InterviewerEmail { get => interviewerEmail; set => interviewerEmail = value; }
+        public int InterviewerPhoneNo { get => interviewerPhoneNo; set => interviewerPhoneNo = value; }
+        public int InterviewerChosenTemplateID { get => interviewerChosenTemplateID; set => interviewerChosenTemplateID = value; }
+        public string InterviewerDay { get => interviewerDay; set => interviewerDay = value; }
+    }
+
+    public class createFeedback
+
+    {
+        public int insertInterviewer(MySqlConnection connect, CreateFeedback sendData)
+        {
+            string interviewerAddSQL = "INSERT INTO interviewer (templateID ,lastName, firstName, address, position, email, phoneNo) "
+                + " VALUES ('"+sendData.InterviewerChosenTemplateID+ "','"+sendData.InterviewerLastName+ "' , '" +sendData.InterviewerFirstName+ 
+                "', '" +sendData.InterviewerAddress+ "' , '" +sendData.InterviewerPosition+ 
+                "','" +sendData.InterviewerEmail+ "','" +sendData.InterviewerPhoneNo+ "')";
+                
+
+            MySqlCommand command = new MySqlCommand(interviewerAddSQL, connect);
+
+            return command.ExecuteNonQuery();
+
+        }
+      
+    }
+}
---
+++ Interviewer/Interviewer_GUI.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Assignment_InterviewerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.idSearch = new System.Windows.Forms.TextBox();
+            this.IDTemplateLabel = new System.Windows.Forms.Label();
+            this.searchIDButton = new System.Windows.Forms.Button();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.templateDataGrid = new System.Windows.Forms.DataGridView();
+            this.templateLoadButton = new System.Windows.Forms.Button();
+            this.sendFeedbackbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.templateDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BACK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Fill In The Form Below.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label2.Location = new System.Drawing.Point(413, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(Placeholder)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label3.Location = new System.Drawing.Point(413, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "(Placeholder)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label4.Location = new System.Drawing.Point(413, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "(Placeholder)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label5.Location = new System.Drawing.Point(413, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "(Placeholder)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label6.Location = new System.Drawing.Point(413, 345);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "(Placeholder)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label7.Location = new System.Drawing.Point(413, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "(Placeholder)";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(534, 230);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(165, 20);
+            this.txt1.TabIndex = 9;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(534, 261);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(165, 20);
+            this.txt2.TabIndex = 10;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(533, 289);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(165, 20);
+            this.txt3.TabIndex = 11;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(534, 343);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(165, 20);
+            this.txt5.TabIndex = 12;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(534, 370);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(163, 20);
+            this.txt6.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 443);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 40);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Create The Feedback";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(533, 317);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(164, 20);
+            this.txt4.TabIndex = 15;
+            // 
+            // idSearch
+            // 
+            this.idSearch.Location = new System.Drawing.Point(16, 109);
+            this.idSearch.Name = "idSearch";
+            this.idSearch.Size = new System.Drawing.Size(166, 20);
+            this.idSearch.TabIndex = 16;
+            // 
+            // IDTemplateLabel
+            // 
+            this.IDTemplateLabel.AutoSize = true;
+            this.IDTemplateLabel.Font = new System.Drawing.Font("Comic Sans MS", 10F);
+            this.IDTemplateLabel.Location = new System.Drawing.Point(12, 87);
+            this.IDTemplateLabel.Name = "IDTemplateLabel";
+            this.IDTemplateLabel.Size = new System.Drawing.Size(97, 19);
+            this.IDTemplateLabel.TabIndex = 17;
+            this.IDTemplateLabel.Text = "Template ID:";
+            // 
+            // searchIDButton
+            // 
+            this.searchIDButton.Location = new System.Drawing.Point(206, 107);
+            this.searchIDButton.Name = "searchIDButton";
+            this.searchIDButton.Size = new System.Drawing.Size(75, 23);
+            this.searchIDButton.TabIndex = 18;
+            this.searchIDButton.Text = "Search";
+            this.searchIDButton.UseVisualStyleBackColor = true;
+            this.searchIDButton.Click += new System.EventHandler(this.searchIDButton_Click);
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(534, 397);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(163, 20);
+            this.txt7.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 8.25F);
+            this.label8.Location = new System.Drawing.Point(413, 400);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 15);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "(Placeholder)";
+            // 
+            // templateDataGrid
+            // 
+            this.templateDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.templateDataGrid.Location = new System.Drawing.Point(16, 136);
+            this.templateDataGrid.Name = "templateDataGrid";
+            this.templateDataGrid.Size = new System.Drawing.Size(391, 279);
+            this.templateDataGrid.TabIndex = 21;
+            // 
+            // templateLoadButton
+            // 
+            this.templateLoadButton.Location = new System.Drawing.Point(16, 421);
+            this.templateLoadButton.Name = "templateLoadButton";
+            this.templateLoadButton.Size = new System.Drawing.Size(126, 39);
+            this.templateLoadButton.TabIndex = 22;
+            this.templateLoadButton.Text = "Load Templates";
+            this.templateLoadButton.UseVisualStyleBackColor = true;
+            this.templateLoadButton.Click += new System.EventHandler(this.templateLoadButton_Click);
+            // 
+            // sendFeedbackbtn
+            // 
+            this.sendFeedbackbtn.Location = new System.Drawing.Point(573, 443);
+            this.sendFeedbackbtn.Name = "sendFeedbackbtn";
+            this.sendFeedbackbtn.Size = new System.Drawing.Size(126, 40);
+            this.sendFeedbackbtn.TabIndex = 23;
+            this.sendFeedbackbtn.Text = "Send The Feedback!";
+            this.sendFeedbackbtn.UseVisualStyleBackColor = true;
+            this.sendFeedbackbtn.Click += new System.EventHandler(this.sendFeedbackbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 495);
+            this.Controls.Add(this.sendFeedbackbtn);
+            this.Controls.Add(this.templateLoadButton);
+            this.Controls.Add(this.templateDataGrid);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.searchIDButton);
+            this.Controls.Add(this.IDTemplateLabel);
+            this.Controls.Add(this.idSearch);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Interviewer ";
+            ((System.ComponentModel.ISupportInitialize)(this.templateDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox idSearch;
+        private System.Windows.Forms.Label IDTemplateLabel;
+        private System.Windows.Forms.Button searchIDButton;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView templateDataGrid;
+        private System.Windows.Forms.Button templateLoadButton;
+        private System.Windows.Forms.Button sendFeedbackbtn;
+    }
+}
+
